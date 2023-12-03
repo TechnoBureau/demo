8e1d56d905404fb6fe07dd8ec735deccfd1d9ff2
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2023-12-04 00:55:04 +0530

    first commit

--- scripts/get_image_version.sh
+++ scripts/get_image_version.sh
@@ -10,7 +10,8 @@ json_data="$1"
 image_name="$2"
 
 # Extract the VERSION from the JSON data
-VERSION=$(echo "$json_data" | jq -r ".[] | .[\"$image_name\"].VERSION")
+VERSION=$(echo '$json_data' | jq -r ".[] | .[\"$image_name\"].VERSION")
 
 # Output the version only
-echo "$VERSION"
+echo "VERSION=${VERSION}" >> $GITHUB_OUTPUT
+
