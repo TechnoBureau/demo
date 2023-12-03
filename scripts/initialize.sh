1dad32d550166b2a9f2b14b8e6447070d2a2eb0f
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2023-12-03 23:59:11 +0530

    first commit

---
+++ scripts/initialize.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+GITHUB_REF=$1
+GITHUB_REF_TYPE=$2
+INPUT_VERSION=$3
+
+cd builders
+result=""
+result1=""
+
+########## GENERAL VERSION
+if [ -n "$INPUT_VERSION" ]; then
+  GENERAL_VERSION="$INPUT_VERSION"
+elif [[ "$GITHUB_REF" == "refs/heads/main" ]]; then
+  GENERAL_VERSION="${MAJOR}.${MINOR}.${FIX}"
+elif [[ "$GITHUB_REF_TYPE" == "tag" ]]; then
+  GENERAL_VERSION=$(echo "$GITHUB_REF" | sed 's/^v//')
+else
+  GENERAL_VERSION="$GITHUB_REF"
+fi
+echo "general_version=${GENERAL_VERSION}"
+
+for d in * ; do
+  if [ -d "$d" ] && [ ! -f "$d/skip" ]; then
+    # Check if Dockerfile has a version specified
+    #VERSION=$(grep -E '^ARG VERSION=' "$d/$d.Dockerfile" | awk '{print $3}')
+    if [ -z "$VERSION" ]; then
+      # Use the general version if not specified in Dockerfile
+      VERSION="${GENERAL_VERSION}"
+    fi
+
+    if [ -z "$result" ]; then
+      result="\"$d\""
+      result1="{ \"$d\": { \"version\": \"$VERSION\" } }"
+    else
+      result+=",\"$d\""
+      result1+=", \"$d\": { \"version\": \"$VERSION\" }"
+    fi
+  fi
+done
+
+echo "images=[$result]"
+echo "images_metadata=[$result1]"
