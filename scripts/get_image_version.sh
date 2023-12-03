23e332c1edba11f8fb286955c36bc9d55b51e2fb
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2023-12-04 00:41:02 +0530

    first commit

---
+++ scripts/get_image_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <json_data> <image_name>"
+  exit 1
+fi
+
+json_data="$1"
+image_name="$2"
+
+# Extract the VERSION from the JSON data
+VERSION=$(echo "$json_data" | jq -r ".[] | .[\"$image_name\"].VERSION")
+
+# Output the version only
+echo "$VERSION"
