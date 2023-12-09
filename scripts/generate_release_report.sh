4dbbcb50193d1b68066e631e3a998b016d3ea6aa
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2023-12-10 01:11:31 +0530

    helo

--- scripts/generate_release_report.sh
+++ scripts/generate_release_report.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Usage: generate_release_report.sh <images_metadata_json> <release_version> <github_token>
-metadata=$(jq -r '.[]' <<< "$1")
-RELEASE_VERSION="$2"
-GITHUB_TOKEN="$3"
+# Usage: generate_release_report.sh <release_version> <github_token>
+RELEASE_VERSION="$1"
+GITHUB_TOKEN="$2"
 #GITHUB_REPOSITORY="Technobureau/demo"
 # Initialize release report and header
 RELEASE_REPORT="#  Released Packages $RELEASE_VERSION \n"
@@ -27,19 +26,12 @@ if [[ ! -z "$ASSET_ID" ]]; then
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/$GITHUB_REPOSITORY/releases/assets/$ASSET_ID > $ASSET_NAME
 
-  existing_data=$(cat $ASSET_NAME)
-
-# Use jq to merge and update report
-merged_data=$(echo "$existing_data" "$metadata" | jq -s '.[0] * .[1]')
-
-jq '.' > "$ASSET_NAME" <<< "$merged_data"
-
 # Loop through image data and update report
-for image in $(jq -r 'keys[]' <<< "$merged_data"); do
+for image in $(jq -r 'keys[]' < $ASSET_NAME); do
   # Extract image details
-  BUILD_TAG=$(echo "$merged_data" | jq -r ".[\"$image\"].BUILD_TAG")
-  IMAGE_ID=$(echo "$merged_data" | jq -r ".[\"$image\"].IMAGE_ID")
-  CREATED=$(echo "$merged_data" | jq -r ".[\"$image\"].CREATED")
+  BUILD_TAG=$(jq -r ".[\"$image\"].BUILD_TAG" < $ASSET_NAME)
+  IMAGE_ID=$(jq -r ".[\"$image\"].IMAGE_ID" < $ASSET_NAME)
+  CREATED=$(jq -r ".[\"$image\"].CREATED" < $ASSET_NAME)
 
   # Update report with extracted details
   RELEASE_REPORT+="\n|$image|$BUILD_TAG|$IMAGE_ID|$CREATED|"
@@ -52,18 +44,5 @@ echo -e "\n"
 # Update release body on GitHub
 curl -s -X PATCH -H "Authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -d '{"body": "'"$RELEASE_REPORT"'"}' "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID" > /dev/null
 
-#If existing asset exists, delete it
-curl -s -L -X DELETE \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/releases/assets/$ASSET_ID > /dev/null
-
-# Upload updated data as new asset
-curl -s -L -X POST \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -H "Content-Type: application/vnd.github+json" \
-    --data-binary "@$ASSET_NAME" \
-    https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/$RELEASE_ID/assets?name=$ASSET_NAME > /dev/null
 fi
 exit 0
